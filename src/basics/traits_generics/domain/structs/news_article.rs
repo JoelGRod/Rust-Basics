3b960795403148c5fa2ea41adba89284ca5cd9bb
joelgrod <joelgrod@gmail.com>
2022-04-05 17:51:17 +0100

    generics, traits

--- src/basics/traits_generics/domain/structs/news_article.rs
+++ src/basics/traits_generics/domain/structs/news_article.rs
@@ -17,3 +17,14 @@ impl Summary for NewsArticle {
         format!("{}, by {} - {}", self.headline, self.author, self.location)
     }
 }
+
+/*
+    One restriction to note with trait implementations is that we can 
+    implement a trait on a type only if at least one of the trait or 
+    the type is local to our crate. 
+    For example, we can implement standard library traits like Display 
+    on a custom type like Tweet as part of our aggregator crate functionality, 
+    because the type Tweet is local to our aggregator crate. 
+    We can also implement Summary on Vec<T> in our aggregator crate, 
+    because the trait Summary is local to our aggregator crate.
+*/
