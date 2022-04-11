a2988054a541d81ece95669d9edf6f9edcf110f2
JoelGRod <joelgrod@gmail.com>
2022-04-11 09:48:02 +0100

    console app

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
+use std::env;
+
 // Entry Point
 fn main() {
-    basics_rust::collections_example();
-    basics_rust::errors_example();
-    basics_rust::structs_example();
-    basics_rust::traits_example();
+    // Basics
+    // basics_rust::collections_example();
+    // basics_rust::errors_example();
+    // basics_rust::structs_example();
+    // basics_rust::traits_example();
+
+    // minigrep
+    let args: Vec<String> = env::args().collect();
+    println!("{:?}", args);
 }
